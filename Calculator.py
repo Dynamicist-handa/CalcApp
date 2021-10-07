999faf2f96bfaaa0ada690288b58bb8fca5d51d3
shreyasrd31@gmail.com <84516626+SRDdev@users.noreply.github.com>
2021-10-07 12:50:58 +0530

    Changed the look of the calculator & fixed the addition issue

--- Calculator.py
+++ Calculator.py
@@ -2,8 +2,8 @@ from tkinter import*
 root = Tk()
 
 # Text Input
-e = Entry(root, width=50, border=10)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+e = Entry(root, width=20, border=5)
+e.grid(row=0, column=0, columnspan=3, padx=0, pady=0)
 
 
 def num_input(number):
@@ -25,22 +25,22 @@ def equals():
 
 
 # Button 9-0 , add ,sub, clear ,equal
-button9 = Button(root, text="9", padx=30, pady=30, command=lambda: num_input(9)).grid(row=1, column=0)
-button8 = Button(root, text="8", padx=30, pady=30, command=lambda: num_input(8)).grid(row=1, column=1)
-button7 = Button(root, text="7", padx=30, pady=30, command=lambda: num_input(7)).grid(row=1, column=2)
+button9 = Button(root, text="9", padx=20, pady=20, command=lambda: num_input(9)).grid(row=1, column=0)
+button8 = Button(root, text="8", padx=20, pady=20, command=lambda: num_input(8)).grid(row=1, column=1)
+button7 = Button(root, text="7", padx=20, pady=20, command=lambda: num_input(7)).grid(row=1, column=2)
 
-button6 = Button(root, text="6", padx=30, pady=30, command=lambda: num_input(6)).grid(row=2, column=0)
-button5 = Button(root, text="5", padx=30, pady=30, command=lambda: num_input(5)).grid(row=2, column=1)
-button4 = Button(root, text="4", padx=30, pady=30, command=lambda: num_input(4)).grid(row=2, column=2)
+button6 = Button(root, text="6", padx=20, pady=20, command=lambda: num_input(6)).grid(row=2, column=0)
+button5 = Button(root, text="5", padx=20, pady=20, command=lambda: num_input(5)).grid(row=2, column=1)
+button4 = Button(root, text="4", padx=20, pady=20, command=lambda: num_input(4)).grid(row=2, column=2)
 
-button3 = Button(root, text="3", padx=30, pady=30, command=lambda: num_input(3)).grid(row=3, column=0)
-button2 = Button(root, text="2", padx=30, pady=30, command=lambda: num_input(2)).grid(row=3, column=1)
-button1 = Button(root, text="1", padx=30, pady=30, command=lambda: num_input(1)).grid(row=3, column=2)
+button3 = Button(root, text="3", padx=20, pady=20, command=lambda: num_input(3)).grid(row=3, column=0)
+button2 = Button(root, text="2", padx=20, pady=20, command=lambda: num_input(2)).grid(row=3, column=1)
+button1 = Button(root, text="1", padx=20, pady=20, command=lambda: num_input(1)).grid(row=3, column=2)
 
-button0 = Button(root, text="0", padx=30, pady=30, command=lambda: num_input(0)).grid(row=4, column=0)
+button0 = Button(root, text="0", padx=20, pady=20, command=lambda: num_input(0)).grid(row=4, column=0)
 
-button_add = Button(root, text="+", padx=30, pady=30, command=lambda: num_input("+")).grid(row=4, column=1)
-button_clear = Button(root, text="clear", padx=30, pady=30, command=lambda: clear_val()).grid(row=5, column=0)
-button_equal = Button(root, text="=", padx=30, pady=30, command=lambda: equals()).grid(row=5, column=1)
+button_add = Button(root, text="+", padx=20, pady=20, command=lambda: num_input("+")).grid(row=4, column=1)
+button_clear = Button(root, text="clear", padx=20, pady=20, command=lambda: clear_val()).grid(row=5, column=0)
+button_equal = Button(root, text="=", padx=20, pady=20, command=lambda: equals()).grid(row=4, column=2)
 
 root.mainloop()
